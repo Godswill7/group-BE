555b868462e57d6bac81a38eb1342fac500d0d18
Godswill Udida <udidagodswill7@gmail.com>
2023-10-18 01:14:15 +0100

    hard things

--- model/userModel.ts
+++ model/userModel.ts
@@ -1,5 +1,26 @@
+import mongoose, { model } from "mongoose";
+
 interface iUser {
     name: string,
     email: string,
     passsword:string
 }
+
+interface iUserData extends iUser, mongoose.Document { }
+
+const userModel = new mongoose.Schema({
+    name: {
+        type:String
+    },
+    email: {
+        type:String
+    },
+    password: {
+        type:String
+    }
+},
+    {
+    timestamps:true
+    })
+
+    export default mongoose.model<iUserData>("users",userModel)
